d4287f37062b9e4e2830d6527134245e7bb3bd39
SirLancelotZ <ngtlancezhu@gmail.com>
2018-09-26 23:14:07 -0700

    edit

--- app/src/main/java/com/example/intentslearning/ReceiveMessageActivity.java
+++ app/src/main/java/com/example/intentslearning/ReceiveMessageActivity.java
@@ -2,14 +2,26 @@ package com.example.intentslearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReceiveMessageActivity extends AppCompatActivity {
+
+    private TextView displayMsg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_message);
+
+        wireWidgets();
+        displayMsg.setText(getIntent().getStringExtra(SendMessageActivity.EXTRA_SENT_MESSAGE));
     }
+
+    private void wireWidgets() {
+        displayMsg = findViewById(R.id.textView_receivemessage_displayMsg);
+    }
+
 
 
 }
--- app/src/main/java/com/example/intentslearning/SendMessageActivity.java
+++ app/src/main/java/com/example/intentslearning/SendMessageActivity.java
@@ -1,31 +1,54 @@
 package com.example.intentslearning;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 public class SendMessageActivity extends AppCompatActivity {
 
-    private Button buttonSendMessage;
-    private EditText editTextMessage;
-    private Button buttonShare;
+    private EditText message;
+    private Button send;
+    private Button share;
 
-
+    public static final String EXTRA_SENT_MESSAGE = "the message";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_message);
+
+        wireWidgets();
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get the text from the edittext
+                String msg = message.getText().toString();
+                //create an intent: where the msg is coming from and where is it going
+                Intent intentSendMsg = new Intent(SendMessageActivity.this, ReceiveMessageActivity.class);
+                //package the text into the intent
+                intentSendMsg.putExtra(EXTRA_SENT_MESSAGE, msg);
+                //start the new activity
+                startActivity(intentSendMsg);
+
+            }
+        });
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //create an intent with no arguments
+                Intent intentShare = new Intent();
+                intentShare.setType("text/plain");
+                intentShare.putExtra(EXTRA_SENT_MESSAGE, message.getText().toString());
+                startActivity(intentShare);
+            }
+        });
     }
 
-    private void wireWidgets(){
-        buttonSendMessage = findViewById(R.id.button_sendmessage_msg);
-        editTextMessage = findViewById(R.id.editText_sendmessage_msg);
+    private void wireWidgets() {
+        message = findViewById(R.id.editText_sendmessage_msg);
+        send = findViewById(R.id.button_sendmessage_send);
+        share = findViewById(R.id.button_sendmessage_share);
     }
-
-    ButtonSendMessage.setOn
-
-
-    intentSendMessage.putExtra("themesage", message);
 }
